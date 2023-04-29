9c824a0d10530513c6e3d9345bcba2b89b1e22f0
Rockydog90210 <130509249+Rockydog90210@users.noreply.github.com>
2023-04-28 18:29:23 -0700

    Add files via upload

--- myScripts.js
+++ myScripts.js
@@ -1,4 +1,4 @@
-            let totalMoney = 0;
+			let totalMoney = 0;
             let clickPower = 1;
             let autoClickPower = 0;
             x1 = 50;
@@ -7,11 +7,47 @@
 			
 			y1 = 30;
 			y5 = 300;
+			y10 = 850;
             dragonFrame = 1; // so the computer knows witch frame to display when you click
-           AC = false;
+            AC = false;
+			updateBackGround()
             
+			function updateBackGround(){
+				if (totalMoney < x1){
+					document.getElementById("clickPowerButtonX1").style.backgroundColor = "#0F0F43";
+				}else if (totalMoney >= x1) {
+					document.getElementById("clickPowerButtonX1").style.backgroundColor = "darkblue";
+				}
+				
+				if (totalMoney < x5){
+					document.getElementById("clickPowerButtonX5").style.backgroundColor = "#0F0F43";
+				}else if (totalMoney >= x5) {
+					document.getElementById("clickPowerButtonX5").style.backgroundColor = "darkblue";
+				}
+				if (totalMoney < x10){
+					document.getElementById("clickPowerButtonX10").style.backgroundColor = "#0F0F43";
+				}else if (totalMoney >= x10) {
+					document.getElementById("clickPowerButtonX10").style.backgroundColor = "darkblue";
+				}
+				if (totalMoney < y1){
+					document.getElementById("autoClickButtonY1").style.backgroundColor = "#0F0F43";
+				}else if (totalMoney >= y1) {
+					document.getElementById("autoClickButtonY1").style.backgroundColor = "darkblue";
+				}
+				
+				if (totalMoney < y5){
+					document.getElementById("autoClickButtonY5").style.backgroundColor = "#0F0F43";
+				}else if (totalMoney >= y5) {
+					document.getElementById("autoClickButtonY5").style.backgroundColor = "darkblue";
+				}
+				if (totalMoney < y10){
+					document.getElementById("autoClickButtonY10").style.backgroundColor = "#0F0F43";
+				}else if (totalMoney >= y10) {
+					document.getElementById("autoClickButtonY10").style.backgroundColor = "darkblue";
+				}
+				
 
-
+			}
             const totMoney = document.getElementById("money");
             const moneyButton = document.getElementById("moneyButton");
             const cp = document.getElementById("power");
@@ -23,6 +59,7 @@
 
             const autoClickerY1 = document.getElementById('autoClickButtonY1');
 			const autoClickerY5 = document.getElementById('autoClickButtonY5');
+			const autoClickerY10 = document.getElementById('autoClickButtonY10');
 			
             const clickPriceX1 = document.getElementById('clickCostX1');
             const clickPriceX5 = document.getElementById('clickCostX5');
@@ -30,7 +67,7 @@
 			
 			const autoPriceY1 = document.getElementById('autoCostY1');
             const autoPriceY5 = document.getElementById('autoCostY5');
-        
+			const autoPriceY10 = document.getElementById('autoCostY10');
 
             doubleClickButtonX1.addEventListener("click", buyClickPowerX1);
             doubleClickButtonX5.addEventListener("click", buyClickPowerX5);
@@ -38,7 +75,8 @@
 
             autoClickerY1.addEventListener("click", autoClickY1);
 			autoClickerY5.addEventListener("click", autoClickY5);
-
+			autoClickerY10.addEventListener("click", autoClickY10);
+	
             moneyButton.addEventListener("click", updateNum); 
             
             
@@ -49,6 +87,7 @@
                 checkClickPower();
                 totMoney.textContent = totalMoney; //updates the number
                 dragonFlap();// makes the draon flap her wings
+				updateBackGround()
             }
 
 
@@ -62,6 +101,7 @@
                 cp.textContent = clickPower;
                 totMoney.textContent = totalMoney;
                 }
+				
             }
 
 
@@ -94,6 +134,7 @@
             }
 			
 			
+			//////////////////////////////////////
 			
 			function checkClickPower(){ 
                 for (var i = 0; i < clickPower; i++){
@@ -107,7 +148,7 @@
 			function autoClickY1(){
 				if (totalMoney >=y1){
 					totalMoney -= y1;
-					y1 = Math.round(y1 * 1.915)
+					y1 = Math.round(y1 * 1.195)
 					++autoClickPower;
 					totMoney.textContent = totalMoney;
 					autoPriceY1.textContent = y1;
@@ -124,7 +165,7 @@
 			function autoClickY5(){
 				if (totalMoney >=y5){
 					totalMoney -= y5;
-					y5 = Math.round(y5 * 1.915)
+					y5 = Math.round(y5 * 1.195)
 					for (var i = 0; i < 5; i++){
                         ++autoClickPower;    
                     }
@@ -140,11 +181,30 @@
 			}
 			
 			
+			function autoClickY10(){
+				if (totalMoney >=y10){
+					totalMoney -= y10;
+					y10 = Math.round(y10 * 1.195)
+					for (var i = 0; i < 10; i++){
+                        ++autoClickPower;    
+                    }
+					totMoney.textContent = totalMoney;
+					autoPriceY10.textContent = y10;
+					ac.textContent = autoClickPower;
+					if (AC <= false){
+						setInterval(autoClick, 1000); 
+						AC = true;
+					}
+					
+				}
+			}
+			
+			
 			
 			function autoClick(){
-				
+				updateBackGround()
 				for (var i = 0; i < autoClickPower; i++){
-                    ++totalMoney; 
+                    ++totalMoney;
 					totMoney.textContent = totalMoney;
                 }
 			}
